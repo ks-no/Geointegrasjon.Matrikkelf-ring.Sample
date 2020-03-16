84c6bdefc8a2253dfff200ac75dcc87f7e9eb45b
Lars Eggan <lars.eggan@norconsult.com>
2020-03-16 15:18:01 +0100

    Oppdatert til .NET framework 4.72 + oppdatert nuget-pakker

--- Geointegrasjon.Matrikkelfoering.Sample/Geointegrasjon.Matrikkelfoering.Sample/GenerateN2N3_NOIS.cs
+++ Geointegrasjon.Matrikkelfoering.Sample/Geointegrasjon.Matrikkelfoering.Sample/GenerateN2N3_NOIS.cs
@@ -4,7 +4,6 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-using no.geointegrasjon.rep.matrikkelfoering;
 
 namespace Geointegrasjon.Matrikkelfoering.SendSample
 {
